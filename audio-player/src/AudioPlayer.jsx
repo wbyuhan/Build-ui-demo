4d44abab62ed55639ace661c40a300b6adfbd0d6
chunsheng.li <chunsheng.li@99bill.com>
2021-08-13 17:05:54 +0800

    audio-player

---
+++ audio-player/src/AudioPlayer.jsx
@@ -0,0 +1,170 @@
+import React, { useState, useEffect, useRef } from "react";
+import AudioControls from "./AudioControls";
+import Backdrop from "./Backdrop";
+import "./styles.css";
+
+const AudioPlayer = ({ tracks }) => {
+  // 状态
+  const [trackIndex, setTrackIndex] = useState(0); // 当前播放的曲目
+  const [trackProgress, setTrackProgress] = useState(0); // 当前进度
+  const [isPlaying, setIsPlaying] = useState(false); // 是否进行播放
+
+  const { title, artist, color, image, audioSrc } = tracks[trackIndex];
+
+  // 存储
+  const audioRef = useRef(new Audio(audioSrc));
+  const intervalRef = useRef();
+  const isReady = useRef(false);
+
+  // 包括 src、currentTime、duration、paused、muted 和 volume
+
+  const { duration } = audioRef.current;
+  //  根据歌曲进度的百分比去变化背景颜色
+  const currentPercentage = duration
+    ? `${trackProgress / duration * 100}%`
+    : "0%";
+  const trackStyling = `
+    -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #fff), color-stop(${currentPercentage}, #777))
+  `;
+  // 交互操作
+
+  const toPrevTrack = () => {
+    // 上一曲
+    if (trackIndex - 1 < 0) {
+      setTrackIndex(tracks.length - 1);
+    } else {
+      setTrackIndex(trackIndex - 1);
+    }
+  };
+
+  const toNextTrack = () => {
+    // 下一曲
+    if (trackIndex < tracks.length - 1) {
+      setTrackIndex(trackIndex + 1);
+    } else {
+      setTrackIndex(0);
+    }
+  };
+
+  // 开始定时器
+
+  const startTimer = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(
+      () => {
+        if (audioRef.current.ended) {
+          toNextTrack();
+        } else {
+          setTrackProgress(audioRef.current.currentTime);
+        }
+      },
+      [1000]
+    );
+  };
+
+  // 暂停/播放
+  useEffect(
+    () => {
+      // content
+      if (isPlaying) {
+        audioRef.current.play();
+        startTimer();
+      } else {
+        clearInterval(intervalRef.current);
+        audioRef.current.pause();
+      }
+      return () => {
+        // clearEffect
+      };
+    },
+    [isPlaying]
+  );
+
+  useEffect(
+    () => {
+      // content
+      audioRef.current.pause();
+      audioRef.current = new Audio(audioSrc);
+      setTrackProgress(audioRef.current.currentTime);
+
+      if (isReady.current) {
+        audioRef.current.play();
+        setIsPlaying(true);
+        startTimer();
+      } else {
+        isReady.current = true;
+      }
+      return () => {
+        // clearEffect
+      };
+    },
+    [trackIndex]
+  );
+
+  // 进度条操作
+  const onScrub = value => {
+    clearInterval(intervalRef.current);
+    audioRef.current.currentTime = value;
+    setTrackProgress(value);
+  };
+  const onScrubEnd = () => {
+    if (!isPlaying) {
+      setIsPlaying(true);
+    }
+    startTimer();
+  };
+
+  // 初始化
+
+  useEffect(() => {
+    // content
+    return () => {
+      // clearEffect
+      audioRef.current.pause();
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  return (
+    <div className="audio-player">
+      <div className="track-info">
+        <img
+          className="artwork"
+          src={image}
+          alt={`track artwork for ${title} by ${artist}`}
+        />
+        <h2 className="title">
+          {title}
+        </h2>
+        <h3 className="artist">
+          {artist}
+        </h3>
+        <AudioControls
+          isPlaying={isPlaying}
+          onPrevClick={toPrevTrack}
+          onNextClick={toNextTrack}
+          onPlayPauseClick={setIsPlaying}
+        />
+        <input
+          type="range"
+          value={trackProgress}
+          step="1"
+          min="0"
+          max={duration ? duration : `${duration}`}
+          className="progress"
+          onChange={e => onScrub(e.target.value)}
+          onMouseUp={onScrubEnd}
+          onKeyUp={onScrubEnd}
+          style={{ background: trackStyling }}
+        />
+      </div>
+      <Backdrop
+        trackIndex={trackIndex}
+        activeColor={color}
+        isPlaying={isPlaying}
+      />
+    </div>
+  );
+};
+
+export default AudioPlayer;
